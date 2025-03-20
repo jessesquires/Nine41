33f31acab0fa02e1b2e2303e8235a3fce09dded1
Joel Fischer <joeljfischer@gmail.com>
2025-03-20 18:25:21 -0400

    Fix timezone and locale (#73)
    Issue #72
    
    Fix the timezone and locale to the device's "current", ensuring that the
    time we set is what we'll see.

--- Sources/main.swift
+++ Sources/main.swift
@@ -14,8 +14,8 @@ import Foundation
 
 let dateFormatter = DateFormatter()
 dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
-dateFormatter.timeZone = TimeZone(abbreviation: "PST")
-dateFormatter.locale = Locale(identifier: "en-US")
+dateFormatter.timeZone = TimeZone.current
+dateFormatter.locale = Locale.current
 
 let isoDateFormatter = ISO8601DateFormatter()
 isoDateFormatter.formatOptions = [
